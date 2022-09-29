8bb3bf4c6b6149b67816f4dc266185a25b1c5702
sswastik02 <sswastik02@gmail.com>
2022-09-29 14:57:19 +0530

    [ADD] Chat History Feature on Frontend
    [+/-] frontend/src/App.tsx              : Chat History Component Included
    [+/-] frontend/src/api/index.ts         : Callback on new message
      [+] frontend/src/components/chats.tsx : Chats component to store chat history

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,25 @@
-import React, {useEffect} from 'react';
-import {connect, send} from "./api"
+import React, { useEffect, useState } from "react";
+import { connect, send } from "./api";
+import { Chats } from "./components/chats";
 
-function sendMsg(){
-  send("Hello")
+function sendMsg() {
+  send("Hello");
 }
 
 function App() {
-  useEffect(connect,[])
+  const [chatHistory, setChatHistory] = useState<Array<string>>([]);
+
+  function updateHistory(msg: string) {
+    const arr: Array<string> = [...chatHistory, msg];
+    setChatHistory(arr);
+  }
+
+  useEffect(() => {
+    connect(updateHistory);
+  }, [chatHistory]); // dependency should be chatHistory as the updateHistory function changes with chatHistory
   return (
     <div className="App">
+      <Chats chatHistory={chatHistory}></Chats>
       <button onClick={sendMsg}>Hit</button>
     </div>
   );
--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -1,31 +1,30 @@
+let backendUrl = process.env.REACT_APP_API_URL;
+let socket = new WebSocket("ws://" + backendUrl + "/ws");
 
-let backendUrl = process.env.REACT_APP_API_URL
-let socket = new WebSocket("ws://" + backendUrl + '/ws')
+function connect(cb: (msg: string) => any) {
+  console.log("Connection on domain: " + backendUrl);
 
-function connect(){
-    console.log("Creating Connection on domain: "+backendUrl)
+  socket.onopen = () => {
+    console.log("Connection Successful");
+  };
 
-    socket.onopen = () => {
-        console.log("Connection Successful")
-    }
+  socket.onmessage = (msg: MessageEvent<any>) => {
+    console.log(msg);
+    cb(msg.data);
+  };
 
-    socket.onmessage = (msg:MessageEvent<any>) => {
-        console.log(msg)
-    }
+  socket.onclose = (close: CloseEvent) => {
+    console.log("Connection Closed : ", close);
+  };
 
-    socket.onclose = (close:CloseEvent) => {
-        console.log("Connection Closed : ",close)
-    }
-
-    socket.onerror = (error:Event) => {
-        console.log("Connection Web Socket Error : ",error)
-    }
-
+  socket.onerror = (error: Event) => {
+    console.log("Connection Web Socket Error : ", error);
+  };
 }
 
-function send(msg:string){
-    console.log("Message Sending : ",msg)
-    socket.send(msg)
+function send(msg: string) {
+  console.log("Message Sending : ", msg);
+  socket.send(msg);
 }
 
-export {connect, send}
+export { connect, send };
